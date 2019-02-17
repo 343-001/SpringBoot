7f7e0ea0999dce3f2aa58094ac192750ed923a34
13392Q <1339271119@qq.comom>
2019-02-17 14:36:37 +0800

    git测试

---
+++ src/main/java/com/qiang/springboot/contorller/HelloContorller.java
@@ -0,0 +1,16 @@
+package com.qiang.springboot.contorller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloContorller {
+
+    @RequestMapping("/getA")
+    @ResponseBody
+    public String A(){
+        System.out.println("sd");
+        return "你就个憨批";
+    }
+}
